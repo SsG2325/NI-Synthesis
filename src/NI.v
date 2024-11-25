6d818a5c6c06a936282d0888906e0d430e3d30a4
SsG2325 <99521671+SsG2325@users.noreply.github.com>
2024-11-26 00:52:25 +0530

    Update NI.v

--- src/NI.v
+++ src/NI.v
@@ -20,7 +20,7 @@ module tt_um_NI(
 	wire [7:0]flit_in;
 	wire flit_in_valid;
 	wire noc_ready;
-	reg [7:0]flit_out;
+	/* verilator lint_off UNUSEDSIGNAL */reg [7:0]flit_out;/* verilator lint_on UNUSEDSIGNAL */
 	reg flit_valid;
 
 	assign rst = ~rst_n;
@@ -34,9 +34,9 @@ module tt_um_NI(
 	assign flit_in = uio_in[7:0];
 	assign	{uo_out[7:5], uio_out, uio_oe}	= data_out[18:0];
 	assign uo_out[4:0] = flit_out[4:0];
-	assign uo_out[0] = proc_ready;
-	assign uo_out[1] = data_valid;
-	assign uo_out[2] = flit_valid;
+	assign uio_out[0] = proc_ready;
+	assign uio_out[1] = data_valid;
+	assign uio_out[2] = flit_valid;
 
 
 	/* verilator lint_off UNUSEDSIGNAL */ wire _unused = &{ena, data_out[31:19], 1'b0}; /* verilator lint_on UNUSEDSIGNAL */
